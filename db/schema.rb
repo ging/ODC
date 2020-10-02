b317ab61e4589c92b5b646f7f08a762fa670f2ec
sonsoleslp <poynting.slp@gmail.com>
2020-10-02 22:44:52 +0200

    Minor fix

--- db/schema.rb
+++ db/schema.rb
@@ -151,7 +151,9 @@ ActiveRecord::Schema.define(version: 2020_10_02_074919) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.string "name"
+    t.string "surname"
     t.string "language"
     t.string "ui_language"
     t.text "tag_array_text", default: ""
