127888e04c135aaa94c63c4524d2fe593c7a7d00
Enrique <ebarra@dit.upm.es>
2021-01-21 18:00:51 +0100

    small change in routes to add a static route for marketing vamping

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ ODC::Application.routes.draw do
 
   #Courses
   resources :courses
+  match '/vamping' => 'courses#show', :defaults => { :id => '45' },  via: [:get]
   match '/webinars', to: 'courses#webinars', via: [:get]
   match '/all_courses', to: 'courses#all_courses', via: [:get]
   match 'courses/:id/enroll' => 'courses#enroll', via: [:post]
