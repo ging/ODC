76d8af76058a89ef8af3356c4559b4fd4e5f9e9a
Enrique <ebarra@dit.upm.es>
2021-02-26 12:51:47 +0100

    recommendations in the lang of the page

--- app/models/recommender_system.rb
+++ app/models/recommender_system.rb
@@ -3,17 +3,19 @@
 class RecommenderSystem
 
   def self.getRecommendCourses(course,n=4)
+    #filter courses and webinars only in the language of the page, or in spanish if page is in english
+    pagelang = (I18n.locale === :en) ? "es":I18n.locale.to_s
     if course.categories.blank?
-      suggestions = default_suggested_courses(course)
+      suggestions = default_suggested_courses(course, pagelang)
     else
-      suggestions = Course.where("webinar = ? and categories is NOT NULL and id != ?", course.webinar, course.id).select{|c| (c.categories & course.categories).length > 0}
-      suggestions = default_suggested_courses(course) if suggestions.blank?
+	    suggestions = Course.where("webinar = ? and categories is NOT NULL and id != ?", course.webinar, course.id).where(:card_lang => pagelang).select{|c| (c.categories & course.categories).length > 0}
+      suggestions = default_suggested_courses(course, pagelang) if suggestions.blank?
     end
     suggestions.sample(n)
   end
 
-  def self.default_suggested_courses(course)
-    Course.where("webinar = ? and id != ?", course.webinar, course.id)
+  def self.default_suggested_courses(course, pagelang="es")
+	  Course.where("webinar = ? and id != ?", course.webinar, course.id).where(:card_lang => pagelang)
   end
 
 end
